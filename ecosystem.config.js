d21759ca023a43f623ac1b153e16df17c4d72f2a
ptendec <sherkhandeus@gmail.com>
2023-02-27 20:53:50 +0600

    feat: added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	apps: [
+		{
+			name: 'NextJS Apps',
+			script: 'node_modules/next/dist/bin/next',
+			args: 'start -p 3000', //running on port 3000
+			cwd: '.',
+			instances: 1,
+			watch: true,
+		},
+	],
+}
